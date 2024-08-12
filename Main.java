4255d0475694b0f7f262e4fe60d8476a2bbdbf25
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2024-08-12 09:39:14 +0300

    added  dataTypesTesting algorithm

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import problems.DataTypes;
+
 public class Main {
     public static void main(String[] args) {
+
+
+
+        //*********************************************************************************
+        //  DATA TYPES:
+        //DataTypes dataTypes = new DataTypes();
+        //dataTypes.dataTypeTesting();
 
     }
 }
---
+++ problems/DataTypes.java
@@ -0,0 +1,46 @@
+package problems;
+
+/*
+HackerRank
+
+Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
+
+A byte is an 8-bit signed integer.
+A short is a 16-bit signed integer.
+An int is a 32-bit signed integer.
+A long is a 64-bit signed integer.
+Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+
+ */
+
+import java.util.Scanner;
+
+public class DataTypes {
+
+    public void dataTypeTesting(){
+        System.out.println("Please enter how many input you want to test! ");
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for( int i=0; i<t ; i++){
+
+            try
+            {
+                System.out.print("Enter the value to be tested: ");
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                //  After these step the number to be entered can not be accdepted as an integer, so please add "L"
+                //  at the end of the number value!
+                if(x>=-2147483648L  && x<=2147483647L)System.out.println("* int");
+                if(x>=-9223372036854775808L  && x<=9223372036854775807L)System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+        }
+    }
+
+}
